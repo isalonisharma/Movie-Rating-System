6900b6dc908836c5eafe44f28898f883361bf74c
Saloni Sharma <saloni.sharma@outlook.in>
2020-06-14 19:26:07 +0530

    removing timeout code

--- movie-catalog-microservice/src/main/java/com/moviecatalogmicroservice/MovieCatalogMicroserviceApplication.java
+++ movie-catalog-microservice/src/main/java/com/moviecatalogmicroservice/MovieCatalogMicroserviceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
@@ -17,9 +16,7 @@ public class MovieCatalogMicroserviceApplication {
 	@LoadBalanced
 	@Bean // producer
 	public RestTemplate getRestTemplate() {
-		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-		clientHttpRequestFactory.setConnectTimeout(3000); // 3 seconds
-		return new RestTemplate(clientHttpRequestFactory);
+		return new RestTemplate();
 	}
 
 	public static void main(String[] args) {
