ee94d8788aecc7ee9728d3f7514529d7f96241b2
Saloni Sharma <saloni.sharma@outlook.in>
2020-06-21 00:29:39 +0530

    account management microservice

---
+++ account-management-microservice/src/test/java/com/accountmanagementmicroservice/AccountManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accountmanagementmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
