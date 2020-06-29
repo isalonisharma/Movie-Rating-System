98f03738c4c27dfc57c65c900c00e89fc0065a2c
Saloni Sharma <saloni.sharma@outlook.in>
2020-06-29 23:21:35 +0530

    back to square 1

---
+++ cloud-configuration-server/src/test/java/com/cloudconfigurationserver/CloudConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudconfigurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
