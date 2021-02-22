37758adb2d4da381d278037895070e2dabf341b4
SaloniSharma0809 <67950658+SaloniSharma0809@users.noreply.github.com>
2021-02-22 10:54:16 +0530

    Update AccountManagementMicroserviceApplication.java

--- account-management-microservice/src/main/java/com/accountmanagementmicroservice/AccountManagementMicroserviceApplication.java
+++ account-management-microservice/src/main/java/com/accountmanagementmicroservice/AccountManagementMicroserviceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class AccountManagementMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AccountManagementMicroserviceApplication.class, args);
 	}
-
 }
